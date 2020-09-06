b19462b2eaa5c345e0b5296b05adaf6362407abe
rysaevrr <renat.rysaev@gmail.com>
2020-09-07 03:07:39 +0500

    Add dev environment.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
+import 'reflect-metadata'
 import express from 'express'
+import { createConnection } from 'typeorm'
+import config from './ormconfig'
 
 const app = express()
 const port = 3000
 
-app.listen(port, () => {
-  return console.log(`server is listening on ${port}`)
+app.listen(port, async () => {
+  try {
+    await createConnection(config)
+    return console.log(`server is listening on ${port}`)
+  } catch (err) {
+    console.error(err)
+  }
 })
