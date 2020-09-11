ca66a69920e38d453351119cb72684c8b44e9c66
rysaevrr <renat.rysaev@gmail.com>
2020-09-11 21:49:28 +0500

    update cd

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 echo $DOCKER_HUB_PASSWORD | docker login --username $DOCKER_HUB_USER --password-stdin
-docker tag remider-api_server "${DOCKER_HUB_USER}/reminder-api_server:last"
+docker tag reminder-api_server "${DOCKER_HUB_USER}/reminder-api_server:last"
 docker push "${DOCKER_HUB_USER}/reminder-api_server:last"
