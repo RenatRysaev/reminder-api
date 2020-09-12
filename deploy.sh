72645e41c64166ba51c8f5b25df6d15a2e1e99d0
rysaevrr <renat.rysaev@gmail.com>
2020-09-12 18:07:55 +0500

    update cd

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
 echo $DOCKER_HUB_PASSWORD | docker login --username $DOCKER_HUB_USER --password-stdin
 docker tag reminder-api_server "${DOCKER_HUB_USER}/reminder-api_server:last"
 docker tag postgres:10.4-alpine "${DOCKER_HUB_USER}/reminder-api_postgres:last"
 docker push "${DOCKER_HUB_USER}/reminder-api_server:last"
 docker push "${DOCKER_HUB_USER}/reminder-api_postgres:last"
+
+eval $(ssh-agent -s)
+echo $DEPLOY_SERVER_SSH_PRIVATE_KEY | tr -d '\r' | ssh-add -
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+ssh deploy@68.183.73.58
