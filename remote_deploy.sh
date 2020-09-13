3ee4bb8f5d6a0e1a09652e1a1c4e4f03715db4a4
rysaevrr <renat.rysaev@gmail.com>
2020-09-14 00:33:34 +0500

    update cd

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -1,3 +1,4 @@
 echo 'success connected'
 echo $DOCKER_HUB_PASSWORD | docker login --username $DOCKER_HUB_USER --password-stdin
+docker-compose down
 docker-compose -f ./docker-compose.prod.yml up -d
