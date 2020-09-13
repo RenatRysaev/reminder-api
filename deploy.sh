82a347744544f513ba67b512e80de4a2c807117c
rysaevrr <renat.rysaev@gmail.com>
2020-09-14 00:18:56 +0500

    update cd

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,6 @@ openssl aes-256-cbc -K $encrypted_db2095f63ba3_key -iv $encrypted_db2095f63ba3_i
 eval "$(ssh-agent -s)"
 chmod 600 /tmp/deploy_rsa
 ssh-add /tmp/deploy_rsa
-ssh deploy@68.183.73.58 bash ./up.sh
+tar -czvf deploy.tar.gz ./docker-compose.prod.yml ./Dockerfile.prod ./remote_deploy.sh
+scp deploy.tar.gz deploy@68.183.73.58/
+ssh deploy@68.183.73.58 "echo 'success connected' && exit"
