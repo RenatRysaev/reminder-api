be00d76a2d27064a5e09639f8fa9eaaf645f2f9c
rysaevrr <renat.rysaev@gmail.com>
2020-09-13 22:53:09 +0500

    update cd

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+echo "Push images to docker hub"
 echo $DOCKER_HUB_PASSWORD | docker login --username $DOCKER_HUB_USER --password-stdin
 docker tag reminder-api_server "${DOCKER_HUB_USER}/reminder-api_server:last"
 docker tag postgres:10.4-alpine "${DOCKER_HUB_USER}/reminder-api_postgres:last"
 docker push "${DOCKER_HUB_USER}/reminder-api_server:last"
 docker push "${DOCKER_HUB_USER}/reminder-api_postgres:last"
 
-#eval $(ssh-agent -s)
-#echo $DEPLOY_SERVER_SSH_PRIVATE_KEY | tr -d '\r' | ssh-add -
-#mkdir -p ~/.ssh
-#chmod 700 ~/.ssh
-#eval "$(ssh-agent -s)"
-#ssh-add ~/.ssh/travis_rsa
-ssh -o StrictHostKeyChecking=no deploy@68.183.73.58 "echo 'success connected' && exit"
+echo "Connect to VPS and up services"
+openssl aes-256-cbc -K $encrypted_db2095f63ba3_key -iv $encrypted_db2095f63ba3_iv -in deploy_rsa.enc -out /tmp/deploy_rsa -d
+eval "$(ssh-agent -s)"
+chmod 600 /tmp/deploy_rsa
+ssh-add /tmp/deploy_rsa
+ssh deploy@68.183.73.58 "echo 'success connected' && exit"
