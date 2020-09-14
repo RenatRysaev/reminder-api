b8e14f4703c85cf7105543aa735b8dfa873934c5
rysaevrr <renat.rysaev@gmail.com>
2020-09-15 03:06:28 +0500

    update cd

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,17 @@ docker push "${DOCKER_HUB_USER}/reminder-api_server:last"
 docker push "${DOCKER_HUB_USER}/reminder-api_postgres:last"
 
 echo "Connect to VPS and up services"
+
+echo "DOCKER_HUB_USER=${DOCKER_HUB_USER}" > sshenv
+echo "DOCKER_HUB_PASSWORD=${$DOCKER_HUB_PASSWORD}" >> sshenv
+
 openssl aes-256-cbc -K $encrypted_db2095f63ba3_key -iv $encrypted_db2095f63ba3_iv -in deploy_rsa.enc -out /tmp/deploy_rsa -d
 eval "$(ssh-agent -s)"
 chmod 600 /tmp/deploy_rsa
 ssh-add /tmp/deploy_rsa
 tar -czvf deploy.tar.gz ./docker-compose.prod.yml ./Dockerfile.prod ./remote_deploy.sh
 scp deploy.tar.gz deploy@68.183.73.58:/home/deploy/
+
 ssh deploy@68.183.73.58 bash << EOF
   tar -xzf ./deploy.tar.gz
   echo $DOCKER_HUB_PASSWORD | docker login --username $DOCKER_HUB_USER --password-stdin
