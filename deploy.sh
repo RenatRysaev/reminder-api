9949f65d984fede20caf2749adf40b52053a630a
rysaevrr <renat.rysaev@gmail.com>
2020-09-20 06:49:23 +0500

    update cd

--- deploy.sh
+++ deploy.sh
@@ -17,11 +17,15 @@ openssl aes-256-cbc -K $encrypted_db2095f63ba3_key -iv $encrypted_db2095f63ba3_i
 eval "$(ssh-agent -s)"
 chmod 600 /tmp/deploy_rsa
 ssh-add /tmp/deploy_rsa
+tar -cvzf docker_compose_configs.tar.gz docker-compose.yml docker-compose.prod.yml
+scp docker_compose_configs.tar.gz deploy@68.183.73.58:/home/deploy/
 
 ssh deploy@68.183.73.58 bash << EOF
-  git clone https://github.com/RenatRysaev/reminder-api.git && cd reminder-api || cd reminder-api && git pull $TRAVIS_BRANCH
+  tar -xzf docker_compose_configs.tar.gz
   echo $DOCKER_HUB_PASSWORD | docker login --username $DOCKER_HUB_USER --password-stdin
   docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
   docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
   docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 EOF
+
+#  git clone https://github.com/RenatRysaev/reminder-api.git && cd reminder-api || cd reminder-api && git pull $TRAVIS_BRANCH
