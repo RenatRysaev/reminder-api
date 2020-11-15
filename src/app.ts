aab7177bebc9057ebb3e40ea25b238848934681d
rysaevrr <renat.rysaev@gmail.com>
2020-11-15 07:57:46 +0500

    [issue-20] Add mysql.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
-import 'reflect-metadata'
 import * as express from 'express'
-import { createConnection } from 'typeorm'
 import * as bodyParser from 'body-parser'
-import config from './ormconfig'
 import { userRoutes } from './features/user/routes'
+import * as mysql from 'mysql2'
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'reminder',
+  port: 3306,
+})
+
+connection.connect((error) => {
+  if (error) {
+    console.log('error', error)
+    return
+  }
+  console.log('Successfully connected to the database.')
+})
 
 const app = express()
 const port = process.env.SERVER_PORT || 3000
@@ -15,7 +29,6 @@ userRoutes(app)
 
 app.listen(port, async () => {
   try {
-    await createConnection(config)
     return console.log(`server is listening on ${port}`)
   } catch (err) {
     console.error(err)
